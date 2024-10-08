e548889010545b60714bab6bec28681a807c7a9a
Shresty Bohra <97122248+ShrestyB@users.noreply.github.com>
2024-10-08 06:54:08 +0530

    Update Subject.java

--- Task1/src/Subject/Subject.java
+++ Task1/src/Subject/Subject.java
@@ -3,11 +3,9 @@ package Subject;
 import observers.Observers;
 
 public interface Subject {
-    void addObservers(Observers Observers);
+   
 
-    void removeObservers(Observers Observers);
-
-    void addObserver(Observers observer);
-    void removeObserver(Observers observer);
+    void addObservers(Observers observer);
+    void removeObservers(Observers observer);
     void notifyObservers(String message);
 }
